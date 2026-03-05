8190b83877807c352ec654f7ae59932798bb1f13
hiiragi-art <shuto.latte@gmail.com>
2026-03-05 17:39:11 +0900

    プレイヤー(仮)のジャンプ

--- GameProject/Project/GameProject/Game/Player.cpp
+++ GameProject/Project/GameProject/Game/Player.cpp
@@ -71,7 +71,7 @@ Player::Player(const CVector3D& pos, bool flip)
 	//座標設定
 	m_pos_old = m_pos = pos;
 	//中心位置設定
-	m_img.SetCenter(100, 200);
+	m_img.SetCenter(90, 180);
 	//反転フラグ
 	m_flip = flip;
 	//通常状態へ
@@ -95,7 +95,7 @@ void Player::StateIdle()
 	//移動フラグ
 	bool move_flag = false;
 	//ジャンプ力
-	const float jump_pow = 5;
+	const float jump_pow = 15;
 
 	//左移動
 	if (HOLD(CInput::eLeft))
@@ -131,6 +131,32 @@ void Player::StateIdle()
 		//反転フラグ
 		move_flag = true;
 	}
+	//SPACEキーでプレイヤーがジャンプ
+	if (m_is_ground && PUSH(CInput::eButton5))
+	{
+		m_vec.y = -jump_pow;
+		m_is_ground = false;
+	}
+	//ジャンプ中なら
+	if (!m_is_ground)
+	{
+		if (m_vec.y > 0)
+			//上昇アニメーション
+			m_img.ChangeAnimation(eAnimJumpUp, false);
+		else
+			//下降アニメーション
+			m_img.ChangeAnimation(eAnimJumpDown, false);
+	}
+	//地面にいるなら
+	else
+	{
+		if (move_flag)
+			//走るアニメーション
+			m_img.ChangeAnimation(eAnimRun);
+		else
+			//待機アニメーション
+			m_img.ChangeAnimation(eAnimIdle);
+	}
 }
 
 void Player::Update()
@@ -163,7 +189,7 @@ void Player::Draw(){
 	//位置設定
 	m_img.SetPos(GetScreenPos(m_pos));
 	//表示サイズ設定
-	m_img.SetSize(200, 200);
+	m_img.SetSize(180, 180);
 	//反転設定
 	m_img.SetFlipH(m_flip);
 	//描画
