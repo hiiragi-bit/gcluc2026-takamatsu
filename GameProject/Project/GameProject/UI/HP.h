eab1e24ae86081c86f9b025e3b80114ccd217e12
tuku-masa <29damasa@gmail.com>
2026-03-12 12:32:40 +0900

    a

--- GameProject/Project/GameProject/UI/HP.h
+++ GameProject/Project/GameProject/UI/HP.h
@@ -1,10 +1,14 @@
 #pragma once
 #include "Base/ObjectBase.h"
+#include"Game/Player.h"
 
 class HP :public ObjectBase {
+public:
 	CImage m_img;
+	int m_chengehp;
 public:
 	HP(const CVector3D& pos);
 	void Update();
 	void Draw();
+	void ChengeHP(int hp);
 };
